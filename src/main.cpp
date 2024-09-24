5509e4c1bfc4f232099c3b0de34742845ca19738
Przemysław Ubuntu <przemyslvw@gmail.com>
2024-09-25 00:24:26 +0200

    pierwsza funkcja

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = LED_BUILTIN;  // Użyj odpowiedniego pinu dla diody LED
+
+// Definiowanie kolorów
+#define COLOR_RESET   "\033[0m"  // Reset koloru
+#define COLOR_RED     "\033[31m"  // Czerwony
+#define COLOR_GREEN   "\033[32m"  // Zielony
+#define COLOR_BLUE    "\033[34m"  // Niebieski
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);  // Ustaw prędkość komunikacji szeregowej
+    pinMode(ledPin, OUTPUT);  // Ustaw pin diody LED jako wyjście
+    Serial.println(COLOR_GREEN "I (0) example: log -> USB" COLOR_RESET);
+    Serial.println("example: print -> stdout");
+    Serial.println("example: print -> stderr");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    // Miganie na czerwono
+    digitalWrite(ledPin, HIGH);  // Włącz diodę
+    Serial.println(COLOR_RED "Dioda LED: Czerwony" COLOR_RESET);
+    delay(5000);
+    digitalWrite(ledPin, LOW);  // Wyłącz diodę
+    delay(500);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    // Dodatkowe logi
+    Serial.println(COLOR_GREEN "I (1437189) example: log -> USB" COLOR_RESET);
+    Serial.println("example: print -> stdout");
+    Serial.println("example: print -> stderr");
+    delay(500);
 }
